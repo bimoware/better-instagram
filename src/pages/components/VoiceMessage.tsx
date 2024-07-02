3e76a922264fbcf1f822aa442684604a9c8b067f
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-07-02 22:53:46 +0100

    Removed useless audio feature

--- src/pages/components/VoiceMessage.tsx
+++ src/pages/components/VoiceMessage.tsx
@@ -1,12 +1,10 @@
 import play from "/icons/play.svg";
 import pause from "/icons/pause.svg";
-import subtitles from "/icons/subtitles.svg";
 import { useRef, useState } from "react";
 import ProgressBar from "./ProgressBar";
 export default function VoiceBubble({ file }: { file: string }) {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [progress, setProgress] = useState(0);
-	const [subtitlesVisible, setSubtitlesVisible] = useState(false);
 	const ref = useRef(null);
 	function setManuelProgress(progress: number) {
 		let audio = ref.current as unknown as HTMLAudioElement;
@@ -39,20 +37,7 @@ export default function VoiceBubble({ file }: { file: string }) {
 					className="select-none hover:cursor-pointer hover:scale-110"
 				/>
 				<ProgressBar progress={progress} setProgress={setManuelProgress} />
-				<img
-					src={subtitles}
-					className="select-none hover:cursor-pointer hover:scale-110"
-					onClick={() => setSubtitlesVisible(!subtitlesVisible)}
-				/>
 			</div>
-			{subtitlesVisible ? (
-				<p className="font-code rounded-2xl py-1 px-4 bg-neutral-900 text-wrap">
-					Sinon tu peux m'envoyer le mail euuh, tu peux envoyer le sujet par mail parceque j'vais
-					travailler sur PC s'il te plait merci
-				</p>
-			) : (
-				<></>
-			)}
 		</div>
 	);
 }
