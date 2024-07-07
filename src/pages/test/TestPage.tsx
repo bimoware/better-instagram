a526d961e7ba7871c24dc1334fbe857ad4b7b457
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-07-07 07:06:53 +0100

    Simple badge test

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,3 +1,10 @@
+import Badge from "../../components/Badge";
+import pfp from "/images/1.png";
+import badge from "/icons/phone.svg";
 export default function TestPage() {
-	return <></>;
+	return (
+		<Badge badge={badge}>
+			<img className="rounded-full" src={pfp} />
+		</Badge>
+	);
 }
